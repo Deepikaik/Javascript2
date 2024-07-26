f1853aae68219fb7fcd86bdabd6fec7086046916
Deepikaik <deepikabhagavati2001@gmail.com>
2024-07-26 11:52:50 +0530

    'js'

--- Iterative statement/for2.js
+++
@@ -1,7 +0,0 @@
-for (var i=0; i<=50;i=i+5){
-    console.log(i)
-}
-
-for (var j=0;j<=20;j++){
-    console.log(j)
-}
--- Iterative statement/two2.js
+++
@@ -1,7 +0,0 @@
-for (var i=1; i<=10;i++){
-    console.log("5*",i,"=",5*i)
-}
-
-for (var j=1; j<=10;j++){
-    console.log(`5* ${j} = ${5*j}`)
-}
